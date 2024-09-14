f58ac948a03703ead3fc9d3fcd20dc3be87f1f1e
jupiter <jvitualla@dnamicro.com>
2024-09-15 07:10:38 +0800

    fixed indent

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 // App.tsx
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {AuthProvider} from './src/providers/AuthProvider';
-import {TailwindProvider} from 'tailwind-rn';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/providers/AuthProvider';
+import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import RootNavigator from './src/navigator/RootNavigator';
 import { AppProvider } from './src/providers/AppProvider';
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <TailwindProvider utilities={utilities}>
           <AuthProvider>
             <NavigationContainer>
-                <RootNavigator />
+              <RootNavigator />
             </NavigationContainer>
           </AuthProvider>
         </TailwindProvider>
